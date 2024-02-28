463ac93177c6fd6a8d0ace10b8f62bd8da1e8cf2
kryvo <kryvo@DESKTOP-F33ATL3>
2024-02-28 18:52:54 +0000

    Atualização de commit

---
+++ src/m17b_reposicao/Main.java
@@ -0,0 +1,15 @@
+package m17b_reposicao;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangulo t = new Triangulo(3, 4f);
+        
+        Quadrado r = new Quadrado(4, 2.8f);
+
+        System.out.println(t);
+        System.out.println(r);
+
+    }
+    
+}
